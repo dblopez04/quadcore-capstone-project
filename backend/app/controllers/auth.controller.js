255c955b2e1bb98fb0cc3c55e4bee36db6d4b1da
daniel <daniel@xtal.gay>
2025-10-31 13:50:20 -0500

    Created a simple authentication API - Added a controller with functions for login and registration - Created endpoints to the API functions using a router

---
+++ backend/app/controllers/auth.controller.js
@@ -0,0 +1,53 @@
+const db = require("../models");
+const User = db.User;
+const Student = db.Student;
+const Faculty = db.Faculty;
+const Visitor = db.Visitor;
+
+var bcrypt = require("bcryptjs");
+
+exports.register = (req, res) => {
+    User.create({
+        user_id: req.body.user_id,
+        email: req.body.email,
+        password_hash: bcrypt.hashSync(req.body.password, 10),
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        phone_number: req.body.phone_number,
+        role: req.body.role
+    }).then(() => {
+        res.send({message: "User registered successfully!"});
+    });
+}
+
+exports.login = (req, res) => {
+    User.findOne({
+        where: {
+            user_id: req.body.user_id
+        }
+    }).then(user =>{
+        if(!user) {
+            return res.status(404).send({ message: "User not found" });
+        }
+
+        var validPassword = bcrypt.compareSync(
+            req.body.password_hash,
+            user.password_hash
+        );
+
+        if (!validPassword) {
+            return res.status(401).send({
+                accessToken: null,
+                message: "Invalid Password!"
+            });
+        }
+
+        res.status(200).send({
+          id: user.id,
+          username: user.username,
+          email: user.email,
+        });
+    }).catch(err => {
+        res.status(500).send({ message: err.message });
+    });
+}
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const db = require("./app/models/index");
-
-
 const app = express();
+
+const authRoutes = require('./app/routes/auth.routes');
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+
+app.use('/api/auth', authRoutes);
 
 app.get("/", (req, res) => {
     res.json({ message: "Hello!" })
